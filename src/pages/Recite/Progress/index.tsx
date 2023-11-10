78fe06020c6498d2d58681346a9073f9069f34c2
yinxing <yinxing@bksp.tech>
2023-11-10 09:40:43 +0800

    hide progress if no value

--- src/pages/Recite/Progress/index.tsx
+++ src/pages/Recite/Progress/index.tsx
@@ -5,10 +5,14 @@ interface ProgressProps {
 const Progress = ({ current, total }: ProgressProps) => {
 	const currentIndex = current - 1;
 	const innerWidth = (currentIndex / total) * 100;
+
+	const hasValue = innerWidth > 0;
+	const outerBackgroundColor = hasValue ? 'bg-slate-100' : 'bg-transparent';
+
 	return (
 		<div className="flex items-center gap-1 justify-center">
 			{/* Outer */}
-			<div className="w-48 h-1.5 bg-slate-100 rounded-full">
+			<div className={`w-48 h-1.5 rounded-full ${outerBackgroundColor} transition-all duration-1000`}>
 				{/* Inner */}
 				<div
 					className="h-full bg-slate-300 rounded-full transition-all duration-300"
