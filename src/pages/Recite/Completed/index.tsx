972eb20ec415f9a2dc54bad6df446b6777f275ac
Xing <yinxingdyx@163.com>
2023-12-08 23:53:35 +0800

    add hotkey for completed page

--- src/pages/Recite/Completed/index.tsx
+++ src/pages/Recite/Completed/index.tsx
@@ -1,9 +1,29 @@
 import { MdDone } from "react-icons/md"
+import { OneKey, TwoKey } from "../WorkArea/keys"
+import { useEffect } from "react"
 
 interface CompletedProps {
   onCrazyPracticeClick: () => void
 }
 const Completed = ({ onCrazyPracticeClick }: CompletedProps) => {
+  const reload = () => {
+    window.location.reload()
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "1") {
+        reload()
+      } else if (e.key === "2") {
+        onCrazyPracticeClick()
+      }
+    }
+    window.addEventListener("keydown", handleKeyDown)
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [])
+
   return (
     <div className="flex flex-col items-center">
       {/* completed icon */}
@@ -12,18 +32,20 @@ const Completed = ({ onCrazyPracticeClick }: CompletedProps) => {
       </div>
       <div className="mt-16">
         <div
-          className="cursor-pointer text-slate-500 px-3 py-2  shadow"
+          className="cursor-pointer text-slate-500 px-3 py-2  shadow justify-center flex items-center gap-x-1"
           onClick={() => {
-            window.location.reload()
+            reload()
           }}
         >
           再来一组
+          <OneKey />
         </div>
         <div
-          className="mt-4 cursor-pointer text-slate-500 px-3 py-2 shadow"
+          className="mt-4 cursor-pointer text-slate-500 px-3 py-2 shadow justify-center flex items-center gap-x-1"
           onClick={onCrazyPracticeClick}
         >
           利用<span className="highlighter">钩子</span>疯狂复习
+          <TwoKey />
         </div>
       </div>
     </div>
