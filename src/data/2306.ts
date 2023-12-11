94a0b9c2332ae7d7c7bf74896aba243d8a49cb21
yinxing <yinxing@bksp.tech>
2023-12-11 10:35:34 +0800

    feat: update strings

--- src/data/2306.ts
+++ src/data/2306.ts
@@ -1,85 +1,101 @@
 import { tStringItem } from ".";
 
 const strings: tStringItem[] = [
-  {
-    en: "While there’s no one agreed-upon answer, most people would say that finding meaning in life involves defining and pursuing what’s important and valuable to you, like your relationships, your goals and your values.",
-    zh: "虽然没有一个公认的答案，但大多数人会说，找到生活的意义包括定义和追求对你重要和有价值的东西，比如你的关系、你的目标和你的价值观",
-    id: '410828e8-43e4-4a35-a2b1-32bd0e407c69',
+	{
+		en: "While there’s no one agreed-upon answer, most people would say that finding meaning in life involves defining and pursuing what’s important and valuable to you, like your relationships, your goals and your values.",
+		zh: "虽然没有一个公认的答案，但大多数人会说，找到生活的意义包括定义和追求对你重要和有价值的东西，比如你的关系、你的目标和你的价值观",
+		id: "410828e8-43e4-4a35-a2b1-32bd0e407c69",
+		hook: {
+			text: "人生意义",
+		},
+	},
+	{
+		en: "I totally get it",
+		zh: "我完全理解",
+		id: "b9a8f1f3-7b1e-4c2f-8d0d-7f0a5f4b0b8f",
+		hook: {
+			text: "理解",
+		},
+	},
+	{
+		en: "The air quality in this city is really poor today, we should consider wearing masks when we go outside. 😷🌫️",
+		zh: "今天这个城市的空气质量真的很差，我们出门的时候应该考虑戴口罩。😷🌫️",
+		id: "5d5d1cc8-24a7-4c12-9610-e2cec4c18dac",
+		hook: {
+			text: "空气质量",
+			imgUrl: "https://s2.loli.net/2023/11/14/jAuWflhSL5HD9O3.png",
+		},
+	},
+	{
+		en: "I have to do the dishes before I can relax. 🍽️💦",
+		zh: "我必须先洗碗才能放松。🍽️💦",
+		id: "e64f1a60-d8ad-47c3-ad29-b456521468f0",
+		hook: {
+			text: "洗碗",
+			imgUrl: "https://s2.loli.net/2023/11/17/QxJoGdSkOUH8y1j.png",
+		},
+	},
+	{
+		en: "I had a bowl of porridge for breakfast this morning. 🍲",
+		zh: "今天早上我吃了一碗粥。🍲",
+		id: "19fa7292-8909-47ac-b2bb-7df241404c97",
+		hook: {
+			text: "粥",
+			imgUrl: "https://s2.loli.net/2023/11/17/afANDuZbHzjMgyW.png",
+		},
+	},
+	{
+		en: "I love adding blueberries to my morning oatmeal for a sweet and healthy breakfast! 🫐🥣",
+		zh: "我喜欢在早餐的燕麦粥里加入蓝莓，这样既甜美又健康！🫐🥣",
+		id: "c3c8e518-f4c8-4af8-8c2d-802ce8f78542",
+		hook: {
+			text: "蓝莓",
+			imgUrl: "https://s2.loli.net/2023/11/17/uQXLPaEUNxovdzc.png",
+		},
+	},
+	{
+		en: "I need to buy new socks for the winter.",
+		zh: "我需要为冬天买新袜子",
+		id: "450263d5-2b18-4cef-8de0-635b885ea015",
+		hook: {
+			text: "袜子",
+			imgUrl: "https://s2.loli.net/2023/11/17/icOz5VA4yxhkdmR.png",
+		},
+	},
+	{
+		en: "I love visiting the aquarium to see all the colorful fish and marine animals. 🐠🐙🦈🐬🐳🐟",
+		zh: "我喜欢去水族馆看五颜六色的鱼和海洋动物。🐠🐙🦈🐬🐳🐟",
+		id: "ed14b6ca-87df-4a61-9cdc-27d471d0aad1",
+		hook: {
+			text: "水族馆",
+			imgUrl: "https://s2.loli.net/2023/11/17/nawdAVy7Hc9MIJN.png",
+		},
+	},
+	{
+		en: '"I love sipping on hot chocolate during the winter months!" 🍫❤️️☕️',
+		zh: "“我喜欢在冬季喝热巧克力！”🍫❤️️☕️",
+		id: "98e87b16-4691-47c3-9710-684ea6a7b6a6",
+		hook: {
+			text: "热巧克力",
+			imgUrl: "https://s2.loli.net/2023/11/17/uEcvwVNXqWQ9MiB.png",
+		},
+	},
+	{
+		en: "The pizza was divided into eight equal slices.",
+		zh: "比萨被平均分成八片",
+		id: "a83da66f-2bd8-485b-b141-020695d364e1",
     hook: {
-      text: '人生意义'
+      text: '分成'
     }
-  },
+	},
   {
-    en: "I totally get it",
-    zh: "我完全理解",
-    id: 'b9a8f1f3-7b1e-4c2f-8d0d-7f0a5f4b0b8f',
+    en: "The construction company estimated that the project would take six months to complete.",
+    zh: '建筑公司估计这项工程需要六个月才能完成',
+    id: '2848410f-645d-42e0-ba4d-8495437b01f4',
     hook: {
-      text: '理解'
-    }
-  },
-  {
-    en: 'The air quality in this city is really poor today, we should consider wearing masks when we go outside. 😷🌫️',
-    zh: '今天这个城市的空气质量真的很差，我们出门的时候应该考虑戴口罩。😷🌫️',
-    id: '5d5d1cc8-24a7-4c12-9610-e2cec4c18dac',
-    hook: {
-      text: '空气质量',
-      imgUrl: 'https://s2.loli.net/2023/11/14/jAuWflhSL5HD9O3.png'
-    }
-  },
-  {
-    en: 'I have to do the dishes before I can relax. 🍽️💦',
-    zh: '我必须先洗碗才能放松。🍽️💦',
-    id: 'e64f1a60-d8ad-47c3-ad29-b456521468f0',
-    hook: {
-      text: '洗碗',
-      imgUrl: 'https://s2.loli.net/2023/11/17/QxJoGdSkOUH8y1j.png'
-    }
-  },
-  {
-    en: 'I had a bowl of porridge for breakfast this morning. 🍲',
-    zh: '今天早上我吃了一碗粥。🍲',
-    id: '19fa7292-8909-47ac-b2bb-7df241404c97',
-    hook: {
-      text: '粥',
-      imgUrl: 'https://s2.loli.net/2023/11/17/afANDuZbHzjMgyW.png'
-    }
-  },
-  {
-    en: 'I love adding blueberries to my morning oatmeal for a sweet and healthy breakfast! 🫐🥣',
-    zh: '我喜欢在早餐的燕麦粥里加入蓝莓，这样既甜美又健康！🫐🥣',
-    id: 'c3c8e518-f4c8-4af8-8c2d-802ce8f78542',
-    hook: {
-      text: '蓝莓',
-      imgUrl: 'https://s2.loli.net/2023/11/17/uQXLPaEUNxovdzc.png'
-    }
-  },
-  {
-    en: 'I need to buy new socks for the winter.',
-    zh: '我需要为冬天买新袜子',
-    id: '450263d5-2b18-4cef-8de0-635b885ea015',
-    hook: {
-      text: '袜子',
-      imgUrl: 'https://s2.loli.net/2023/11/17/icOz5VA4yxhkdmR.png'
-    }
-  },
-  {
-    en: 'I love visiting the aquarium to see all the colorful fish and marine animals. 🐠🐙🦈🐬🐳🐟',
-    zh: '我喜欢去水族馆看五颜六色的鱼和海洋动物。🐠🐙🦈🐬🐳🐟',
-    id: 'ed14b6ca-87df-4a61-9cdc-27d471d0aad1',
-    hook: {
-      text: '水族馆',
-      imgUrl: 'https://s2.loli.net/2023/11/17/nawdAVy7Hc9MIJN.png'
-    }
-  },
-  {
-    en: '"I love sipping on hot chocolate during the winter months!" 🍫❤️️☕️',
-    zh: '“我喜欢在冬季喝热巧克力！”🍫❤️️☕️',
-    id: '98e87b16-4691-47c3-9710-684ea6a7b6a6',
-    hook: {
-      text: '热巧克力',
-      imgUrl: 'https://s2.loli.net/2023/11/17/uEcvwVNXqWQ9MiB.png'
+      text: '估计'
     }
   }
-]
+];
 
 export default strings;
