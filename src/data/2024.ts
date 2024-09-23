3b926c2c54d0b575e92111971c35cb2a927e4fc3
Xing <yinxingdyx@163.com>
2024-09-23 17:19:56 +0800

    update items

---
+++ src/data/2024.ts
@@ -0,0 +1,54 @@
+import { tStringItem } from ".";
+
+const strings: tStringItem[] = [
+	// {
+	// 	en: "While there’s no one agreed-upon answer, most people would say that finding meaning in life involves defining and pursuing what’s important and valuable to you, like your relationships, your goals and your values.",
+	// 	zh: "虽然没有一个公认的答案，但大多数人会说，找到生活的意义包括定义和追求对你重要和有价值的东西，比如你的关系、你的目标和你的价值观",
+	// 	id: "410828e8-43e4-4a35-a2b1-32bd0e407c69",
+	// 	hook: {
+	// 		text: "人生意义",
+	// 	},
+	// },
+	{
+		en: "Hey, I'm just wondering if you've got a moment to help me with this spreadsheet?",
+		zh: "嘿，我想知道你是否有时间帮我处理这个电子表格？",
+		id: "5ac50525-64ca-4572-ace1-6fb209564d04",
+		hook: {
+			text: "想知道",
+		},
+	},
+	{
+		en: "I'll be there at the game to cheer on our team, and I know they'll give it their all!",
+		zh: "我会在比赛现场为我们的队伍加油，我知道他们会全力以赴！",
+		id: "020e0d32-c06e-47fd-90ce-3e7c5f278bf3",
+		hook: {
+			text: "加油",
+		},
+	},
+	{
+		en: "After working non-stop for hours, I think it's time for me to take a break and grab some lunch.",
+		zh: "连续工作了几个小时后，我想是时候休息一下，去吃午饭了。",
+		id: "3e9d8f42-bf98-4445-a9dc-f5df2f119ac8",
+		hook: {
+			text: "休息",
+		},
+	},
+	{
+		en: "The idea of working from home is nothing new to us; we've been doing it for years.",
+		zh: "在家工作的想法对我们来说并不新鲜；我们已经做了好多年了。",
+		id: "2538632a-995f-4858-a9ed-57f659b7e9b8",
+		hook: {
+			text: "并不新鲜",
+		},
+	},
+	{
+		en: "Even though the deadline is approaching, you still have plenty of time to polish your presentation.",
+		zh: "尽管截止日期即将到来，但你仍有足够的时间来完善你的演示文稿。",
+		id: "f4aa8da5-d1ec-4647-8153-fcb41f79de48",
+		hook: {
+			text: "有足够的时间",
+		},
+	},
+];
+
+export default strings;
